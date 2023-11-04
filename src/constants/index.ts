4dea68d2ef5f0e516251ff0e6e4044771ddca64f
SchnapsterDog <olivertrajceski@yahoo.com>
2023-11-04 03:44:36 +0100

    style: replacing constants with enum inside transitionmap

--- src/constants/index.ts
+++
@@ -1,4 +0,0 @@
-export const TRANSITION_FADE_LEFT = 'fade-left';
-export const TRANSITION_FADE_RIGHT = 'fade-right';
-export const TRANSITION_FADE_UP = 'fade-up'; 
-export const TRANSITION_FADE_DOWN = 'fade-down';
